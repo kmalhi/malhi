8d20e56d655c7c33f9aa57cde9bacb16b803a853
kmalhi <karan.malhi@gmail.com>
2015-01-22 13:38:23 -0800

    Commented out gwt-servlet and gwt-dev as we have added gwt-bootstrap3 Added resty-gwt dependency Added persistence.xml though it is not being used right now Made changes to uibinder related files just to test a few things with layout

--- webapp/src/main/java/org/malhi/client/Main.java
+++ webapp/src/main/java/org/malhi/client/Main.java
@@ -1,12 +1,19 @@
 package org.malhi.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
 
-public class Main implements EntryPoint {
+public class Main extends Composite implements EntryPoint {
+	interface MyUiBinder extends UiBinder<Widget, Main> {
+	}
+
+	private static MyUiBinder myUiBinder = GWT.create(MyUiBinder.class);
 	@Override
 	public void onModuleLoad() {
-		RootPanel.get().add(new Button("Info Portal"));
+		RootPanel.get().add(myUiBinder.createAndBindUi(this));
 	}
 }
